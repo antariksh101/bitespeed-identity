171281f68e89c2e066c9b56ad2c5361c0c790049
antariksh101 <antarikshkahyap157@gmail.com>
2026-03-02 23:39:03 +0530

    Updated server to use dynamic PORT for deployment

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const identifyRoute = require("./routes/identifyRoute");
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Bitespeed Identity Service is running 🚀");
+});
+
 app.use("/", identifyRoute);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
